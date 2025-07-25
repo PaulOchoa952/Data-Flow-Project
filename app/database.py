d5b67dbfe50f83cf5a8008f7733f88e4e259bccc
Paul Ochoa <alejochoa99@gmail.com>
2025-07-25 12:38:25 -0700

    use env

--- app/database.py
+++ app/database.py
@@ -1,11 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 import os
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+asyncpg://myuser:password@localhost:5432/cars_db"
-)
+load_dotenv()  # This loads variables from .env into the environment
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
